71930ef28cacaa83317417410423b16b2cee27ec
Johannes Eichler <jo.eichler@gmx.de>
2015-06-01 02:18:57 +0200

    animation error message improved

---
+++ On the Map/ErrorDialog.swift
@@ -0,0 +1,21 @@
+//
+//  ErrorDialog.swift
+//  On the Map
+//
+//  Created by Johannes Eichler on 31.05.15.
+//  Copyright (c) 2015 Eichler. All rights reserved.
+//
+
+import UIKit
+
+class ErrorDialog: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
