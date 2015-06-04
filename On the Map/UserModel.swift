9d111d6aad622219432df770e3af202515c066f2
Johannes Eichler <jo.eichler@gmx.de>
2015-06-05 00:32:55 +0200

    userModel implemented

---
+++ On the Map/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  On the Map
+//
+//  Created by Johannes Eichler on 04.06.15.
+//  Copyright (c) 2015 Eichler. All rights reserved.
+//
+
+import UIKit
+
+class UserModel: NSObject {
+   
+}
